c412cb76e5534dd9d7063e551effb888a4166469
Nat0110tan <tan00250@umn.edu>
2023-04-14 14:28:15 -0500

    envv

--- src/components/catesFilter.js
+++ src/components/catesFilter.js
@@ -1,18 +1,18 @@
 import Link from "next/link";
 import { useAuth, UserButton, SignIn } from "@clerk/nextjs";
 export default function CatesFilter({ cate, fetchCates, id, type}) {
-  const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
-  const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-  const { isLoaded, userId, sessionId, getToken } = useAuth();
-  const handleFilterDelete = async (e) => {
-    const token = await getToken({ template: "codehooks" });
-    const url = API_ENDPOINT + "/cateslist?cates="+cate;
-    await fetch(url, {
-      method: "DELETE",
-      headers: { "Authorization": "Bearer " + token },
-    });
-    fetchCates(token);
-  };
+  // const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
+  // const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+  // const { isLoaded, userId, sessionId, getToken } = useAuth();
+  // const handleFilterDelete = async (e) => {
+  //   const token = await getToken({ template: "codehooks" });
+  //   const url = API_ENDPOINT + "/cateslist?cates="+cate;
+  //   await fetch(url, {
+  //     method: "DELETE",
+  //     headers: { "Authorization": "Bearer " + token },
+  //   });
+  //   fetchCates(token);
+  // };
 
   return (
     <div>
@@ -23,12 +23,12 @@ export default function CatesFilter({ cate, fetchCates, id, type}) {
         {cate}
       </Link>
 
-      <button
+      {/* <button
         onClick={handleFilterDelete}
         className="text-center bg-blue-400 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full"
       >
         ❌
-      </button>
+      </button> */}
     </div>
   );
 }
